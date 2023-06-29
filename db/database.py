468fb6feece7d6e940e82fc5d8180289eb25f66e
Gopikrishnan Sasikumar <gopikrishnans1996@gmail.com>
2023-06-30 00:05:48 +0530

    take env vars

--- db/database.py
+++ db/database.py
@@ -1,5 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql+psycopg2://your_username:your_password@your_host:your_port/your_database')
+user_name = os.environ.get('POSTGRES_USER')
+password = os.environ.get('POSTGRES_PASSWORD')
+host = os.environ.get('POSTGRES_HOST')
+port = os.environ.get('POSTGRES_PORT')
+database = os.environ.get('POSTGRES_DB')
+
+
+engine = create_engine(f'postgresql+psycopg2://{user_name}:{password}@{host}:{port}/{database}')
 Session = sessionmaker(bind=engine)
